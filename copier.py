20428b353cc3c34266a7b3a3e298b3b5525b277e
tinyOfficeAutomatization <tinyOfficeAutomatization@gmail.com>
2020-10-13 21:42:52 +0300

    copier now can copy file!

--- copier.py
+++ copier.py
@@ -24,10 +24,28 @@ _Файл с настройками:_
 files_to_copy = []
 
 with open('copier.cop') as inf:
-    sourceDir = inf.readline()
-    targetDir = inf.readline()
+    sourceDir = inf.readline().strip()
+    targetDir = inf.readline().strip()
     for line in inf:
         files_to_copy.append(line.strip())
 
+print('sourceDir', sourceDir)
+print('targetDir', targetDir)
 print(files_to_copy)
+
+# По очереди открывать файлы из списка и сохранять их в другом месте
+
+filename = files_to_copy[0]
+with open(sourceDir + '/' + filename, 'r', encoding="cp1251") as src_f:
+    with open(targetDir + '/' + filename, 'w', encoding="utf-8-sig") as trg_f:
+         for line in src_f:
+             print(line)
+             #trg_f.write(line.decode('cp1251').encode('utf8'))
+             trg_f.write(line)
+
+#f = file("utf8.html", "wb")
+#for line in file("cp1251.html", "rb"):
+#    f.write(line.decode('cp1251').encode('utf8'))
+
+
 
