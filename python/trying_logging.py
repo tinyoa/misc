b8d051bbdcbf2226cf2b1eff07588acfccbb9bbf
tinyOfficeAutomatization <tinyOfficeAutomatization@gmail.com>
2022-11-02 00:47:18 +0300

    trying_logging

--- python/trying_logging.py
+++ python/trying_logging.py
@@ -12,33 +12,49 @@ Created on Tue Nov  1 12:00:32 2022
 
 # spam.log
 
-
-
 import logging
-
-logging.basicConfig(level=logging.DEBUG
-                    , filename='spam.log'
-                    , formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s'))
-logger = logging.getLogger()
-print(logger)
-
-print()
-#logger.setLevel('DEBUG')
-#logging.DEBUG
-
-print(logger.level)     
-#10
-print()
-
-print(logger.handlers)  
-#[<StreamHandler stderr (NOTSET)>]
+import os
 
 def main(name):
-    logger.debug(f'Enter in the main() function: name = {name}')
+    
+    #print(os.path.abspath(os.getcwd()))
+    work_dir = os.path.dirname(os.path.abspath(__file__))
+    #print(work_dir)
+    
+    try:
+        logging.basicConfig(level=logging.DEBUG
+                            , filename=work_dir +'\\spam.log'
+                            , format='%(asctime)s %(clientip)-15s %(name)s - %(levelname)s - %(message)s')
+        logger = logging.getLogger()
+        logger.setFormatter = logging.Formatter('%(asctime)s %(clientip)-15s %(user)-8s %(message)s') 
+        print('logger: ', logger)
+        
+        print()
+        #logger.setLevel('DEBUG')
+        #logging.DEBUG
+        
+        print('logger.level: ', logger.level)
+        #10
+        print()
+        
+        #root.format = logging.Formatter('%(asctime)s %(clientip)-15s %(user)-8s %(message)s') 
+        #print('logger.format: ', logger.format)
+        
+        print('logger.handlers: ', logger.handlers)  
+        #[<StreamHandler stderr (NOTSET)>]
+        
+        logger.debug('Enter in the main() function: name = ')
+
     #DEBUG:root:Enter in the main() function: name = oleg
+    
+    except Exception as e:
+        print(e.__class__)
+    finally:
+        pass
+        #logging.shutdown()
 
 if __name__ == '__main__':
-    main('semen')
+    main('petr')
 
 
 #Раз, два, три, четыря. Пять
--- python/trying_logging_2.py
+++ python/trying_logging_2.py
@@ -6,3 +6,34 @@ Created on Tue Nov  1 22:17:41 2022
 """
 
 
+
+# spam.log
+
+
+
+import logging
+
+logging.basicConfig(level=logging.DEBUG
+                    , filename='spam.log'
+                    , format='%(asctime)s %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger()
+print(logger)
+
+print()
+#logger.setLevel('DEBUG')
+#logging.DEBUG
+
+print(logger.level)     
+#10
+print()
+
+print(logger.handlers)  
+#[<StreamHandler stderr (NOTSET)>]
+
+def main(name):
+    logger.debug(f'Enter in the main() function: name = {name}')
+    #DEBUG:root:Enter in the main() function: name = oleg
+
+if __name__ == '__main__':
+    main('kirll')
+
